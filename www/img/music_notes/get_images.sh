fddd9c22f730c4e21fbf3edbd96ee79d93b0c0f0
Tony Brock <abrock17@gmail.com>
2017-03-06 21:37:00 -0500

    added a bunch of note images; displaying one on the index page

---
+++ www/img/music_notes/get_images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+curl -o Treble-Middle-C.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/Middle-C
+curl -o Treble-D.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/D
+curl -o Treble-E.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/E
+curl -o Treble-F.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/F
+curl -o Treble-G.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/G
+curl -o Treble-A.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/A
+curl -o Treble-B.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/B
+curl -o Treble-highC.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/highC
+curl -o Treble-highD.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/highD
+curl -o Treble-highE.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/highE
+curl -o Treble-highF.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/highF
+curl -o Treble-highG.png https://www.quia.com//files/quia/users/troykay5/musicnotestreble/highG
+
+curl -o Bass-Middle-C.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/Middle-C
+curl -o Bass-D.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/D
+curl -o Bass-E.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/E
+curl -o Bass-F.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/F
+curl -o Bass-G.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/G
+curl -o Bass-A.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/A
+curl -o Bass-B.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/B
+curl -o Bass-highF.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/F_1
+curl -o Bass-highG.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/G_1
+curl -o Bass-highA.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/A_1
+curl -o Bass-highB.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/B_1
+curl -o Bass-Middle-C.png https://www.quia.com//files/quia/users/troykay5/musicnotesbass/C_1
