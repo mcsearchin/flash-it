a097ec802cd2782a6e6c2e38f785b1722ca35df5
Tony Brock <abrock17@gmail.com>
2017-03-20 22:05:13 -0400

    right button hides answer and updates image

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,5 +1,37 @@
-describe("FlashIt", function() {
-  it("exists", function() {
+describe('FlashIt', function() {
+
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = 'base';
+    jasmine.getFixtures().load('www/index.html');
+  });
+
+  it('exists', function() {
     expect(FlashIt).toBeDefined();
+  });
+
+  describe('on ready', function() {
+    beforeEach(function() {
+      FlashIt.ready();
+    });
+
+    describe('pressing the right button', function() {
+
+      it('hides the answer div', function() {
+        $('#right-button').click();
+
+        var answer = $('.answer');
+        expect(answer).toBeHidden();
+      });
+
+      it('updates the question image', function() {
+        var previousImage = $('#question-image').attr('src');
+
+        $('#right-button').click();
+
+        var newImage = $('#question-image').attr('src');
+        expect(newImage).toBeTruthy();
+        expect(newImage).not.toEqual(previousImage);
+      });
+    });
   });
 });
--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,10 @@
 var FlashIt = {};
+
+FlashIt.ready = function() {
+  $('#right-button').click(function() {
+    $('.answer').hide();
+    $('#question-image').attr('src', 'img/music_notes/Treble-D.png')
+  });
+}
+
+$(document).ready(FlashIt.ready);
